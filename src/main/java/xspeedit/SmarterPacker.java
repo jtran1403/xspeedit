3aa76ac851aeadd836c521ef419da7259014569b
jtran1403 <jtran1403@gmail.com>
2019-02-14 15:41:55 +0100

    Implement smarter packer packing

--- src/main/java/xspeedit/SmarterPacker.java
+++ src/main/java/xspeedit/SmarterPacker.java
@@ -1,11 +1,16 @@
 package xspeedit;
 
+import xspeedit.domain.Article;
 import xspeedit.domain.StandardPackage;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+//TODO remove duplicated code with simplerPacker
 public final class SmarterPacker extends StandardPacker implements Packer {
+
+    private List<Article> articles;
 
     SmarterPacker() {
         super();
@@ -13,13 +18,35 @@ public final class SmarterPacker extends StandardPacker implements Packer {
 
     @Override
     public List<String> pack(String articleList) {
-        final List<Integer> articles = getArticlesBySize(articleList);
+        articles = getArticlesBySize(articleList).stream()
+                .map(Article::new)
+                .collect(Collectors.toList());
 
         articles.forEach(newArticle -> {
-            final StandardPackage lastPackage = getLastPackage();
-            lastPackage.addNewArticle(newArticle);
+            if (!newArticle.isPacked()) {
+                final StandardPackage lastPackage = getLastPackage();
+                final boolean addedArticle = lastPackage.addNewArticle(newArticle.getSize());
+                if (!addedArticle) {
+                    addInNewPackage(newArticle.getSize());
+                    tryToFillWithRemainingArticles(lastPackage);
+                }
+                newArticle.packed();
+            }
         });
         return getPackagesAsString();
+    }
+
+    private void tryToFillWithRemainingArticles(StandardPackage lastPackage) {
+        final Integer spaceLeft = lastPackage.getSpaceLeft();
+        final Optional<Article> complementaryArticle = articles.stream()
+                .filter(article -> !article.isPacked())
+                .filter(remainingArticle -> spaceLeft.equals(remainingArticle.getSize()))
+                .findFirst();
+        if(complementaryArticle.isPresent()) {
+            final Article article = complementaryArticle.get();
+            lastPackage.addNewArticle(article.getSize());
+            article.packed();
+        }
     }
 
     private List<String> getPackagesAsString() {
@@ -31,5 +58,10 @@ public final class SmarterPacker extends StandardPacker implements Packer {
     private StandardPackage getLastPackage() {
         final int lastPackageIndex = packedArticles.size() - 1;
         return packedArticles.get(lastPackageIndex);
+    }
+
+    private void addInNewPackage(Integer newArticle) {
+        packedArticles.add(new StandardPackage());
+        getLastPackage().addNewArticle(newArticle);
     }
 }
--- src/test/java/xspeedit/SmarterPackerTest.java
+++ src/test/java/xspeedit/SmarterPackerTest.java
@@ -1,5 +1,6 @@
 package xspeedit;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,9 +10,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SmarterPackerTest {
 
+    private SmarterPacker smarterPacker;
+
+    @Before
+    public void setUp() {
+        smarterPacker = new SmarterPacker();
+    }
+
     @Test
     public void should_add_articles_in_package() {
-        final SmarterPacker smarterPacker = new SmarterPacker();
         final ArrayList<String> expectedArticles = new ArrayList<>();
         expectedArticles.add("163");
 
@@ -20,4 +27,14 @@ public class SmarterPackerTest {
         assertThat(packedArticles).containsExactlyElementsOf(expectedArticles);
     }
 
+    @Test
+    public void should_fill_package_with_article_of_complementary_size_when_next_article_is_too_big() {
+        final ArrayList<String> expectedArticles = new ArrayList<>();
+        expectedArticles.add("163");
+        expectedArticles.add("4");
+
+        final List<String> packedArticles = smarterPacker.pack("1643");
+
+        assertThat(packedArticles).containsExactlyElementsOf(expectedArticles);
+    }
 }
