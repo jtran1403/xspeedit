b17856689466abdb9a2723d83b1f2693fe6e5b4a
jtran1403 <jtran1403@gmail.com>
2019-02-14 14:49:41 +0100

    Remove forgotten code in SimplerPackerTest

--- src/test/java/xspeedit/SimplePackerTest.java
+++ src/test/java/xspeedit/SimplePackerTest.java
@@ -20,7 +20,6 @@ public class SimplePackerTest {
 
     @Test
     public void should_add_articles_in_package() {
-        final SimplePacker simplePacker = new SimplePacker();
         final ArrayList<String> expectedArticles = new ArrayList<>();
         expectedArticles.add("163");
 
@@ -31,8 +30,6 @@ public class SimplePackerTest {
 
     @Test
     public void should_add_new_article_in_new_package_if_current_package_cannot_fit_it() {
-        final SimplePacker simplePacker = new SimplePacker();
-
         final List<String> packedArticles = simplePacker.pack("1635");
 
         assertThat(packedArticles).containsExactly("163", "5");
